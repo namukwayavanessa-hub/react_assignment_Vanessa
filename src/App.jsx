9d4816af8303aa8492cd83fc2a1599db7dea7d79
Vanessa <kimoliza321@gmail.com>
2026-06-30 10:12:34 +0300

    added my react assignment

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App ()
+ {
+   return(
+    <div>
+      <h1>MY FAVORITE THING</h1>
+      <h2> NAMUKWAYA KIMO VANESSA</h2>
+      <p> My favorite food: BBQ wings , rice and fries.</p>
+      <p>My favorite hobby: writing novels</p>
+      <p>My favorite movie: Polygmaist</p>
+      <p>My dream career: Data Analyst</p>
+    </div>
+   );
+  }
+export default App;  
